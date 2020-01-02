67f82e395546a3cacc843d5831d316d36ebc1d9d
hummingengineer <hummingengineer@gmail.com>
2020-01-02 21:38:37 +0900

    Home.vue outline

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://opencv.org/wp-content/uploads/2019/02/opencv-logo-1.png" transition="scale-transition" width="40" />
+        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="./assets/Logo.png" width="250" />
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer />
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn href="https://github.com/hummingengineer/image-processing-app" target="_blank" text>
+        <span class="mr-2">개발자 홈페이지</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view :key="$route.fullPath" />
     </v-content>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  name: 'App'
+}
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-file-input v-model="originalFile" class="mt-5" label="파일 선택" outlined prepend-icon="mdi-camera" accept="image/*" :clearable="false" append-outer-icon="mdi-close-circle" @click:append-outer="previewImg = null; originalFile = null" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-subheader>기존 이미지</v-subheader>
+        <v-img v-if="previewImg" :src="previewImg"></v-img>
+      </v-col>
+      <v-col>
+        <v-subheader>변환된 이미지</v-subheader>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+
+  data: function () {
+    return {
+      originalFile: null,
+      convertedFile: null,
+      previewImg: null
+    }
+  },
+
+  watch: {
+    originalFile: function (val) {
+      if(!val) return;
+      let reader = new FileReader()
+      reader.addEventListener('load', () => {
+        this.previewImg = reader.result
+      }, false)
+      reader.readAsDataURL(val)
+    }
   }
+
 }
 </script>
+
+<style>
+
+</style>
