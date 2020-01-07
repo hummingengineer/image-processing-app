b784dbc9698ec56ca70857dbb975569ddfac18f9
hummingengineer <56923582+hummingengineer@users.noreply.github.com>
2020-01-07 11:48:52 +0900

    vue.config For Electron-App's Portable Version
    If you want to add Portable-App's icon, "target": ["portable"], "icon": require('path').join('public', 'icon.png')

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  "pluginOptions": {
+    "electronBuilder": {
+      "builderOptions": {
+        "win": {
+          "target": ["portable"]
+        },
+        "portable": {
+          "artifactName": "Image-Processor_portable_x64.exe"
+        }
+      }
+    }
+  }
 }
